43a49fc3786346a826448644a4813185554c235a
vinaykiranks <vinaykiranks886@gmail.com>
2017-01-27 00:00:17 +0530

    Add files via upload

---
+++ inc.java
@@ -0,0 +1,21 @@
+public class inc
+{
+static int b=10;
+  public static void main(String s[])
+   {
+     int a=20;
+       
+       int b= a-- - --a;
+int c=a--;
+int d=a>>2;
+int e=a&b;
+
+System.out.println("a is "+a);
+System.out.println("b is "+b);
+System.out.println("c is "+c);
+System.out.println("d is "+d);
+System.out.println("e is "+e);
+
+
+  }
+}
